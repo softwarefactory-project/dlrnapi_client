e0fd3c7a142842d635de4d085252119a0043154a
Fabien Boucher <fboucher@redhat.com>
2018-04-10 14:45:30 +0200

    Add SSL_VERIFY env var support to deactivate SSL cert check
    This kind of option is always useful in testing environment.
    
    Change-Id: I3ae9dcb49757aa75dd0b18e18241fad291aaf5d2

--- dlrnapi_client/configuration.py
+++ dlrnapi_client/configuration.py
@@ -15,6 +15,7 @@ from __future__ import absolute_import
 
 import logging
 import sys
+import os
 import urllib3
 
 from six import iteritems
@@ -76,7 +77,10 @@ class Configuration(object):
         # SSL/TLS verification
         # Set this to false to skip verifying SSL certificate when calling API
         # from https server.
-        self.verify_ssl = True
+        if os.environ.get('SSL_VERIFY') == '0':
+            self.verify_ssl = None
+        else:
+            self.verify_ssl = True
         # Set this to customize the certificate file to verify the peer.
         self.ssl_ca_cert = None
         # client certificate file
